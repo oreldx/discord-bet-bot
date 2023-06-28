1c6af51865691e2dad74a96ab61666f1001691ad
orel <orel.dev@proton.me>
2023-06-28 18:37:05 +0200

    🐛 ✨ ADD show command, UPDATE storage, FIX only ASCII

--- cogs/bet.py
+++ cogs/bet.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import Embed
 from dotenv import dotenv_values
 
 import os
@@ -24,11 +25,10 @@ class Bet(commands.Cog):
         author = str(ctx.author.id)
 
         if author not in stored_bets:
-            stored_bets[author] = {}
-        
-        if self.hash_function.hexdigest() not in stored_bets[author]:
-            self.hash_function.update(message.encode('ascii'))
-            stored_bets[author][self.hash_function.hexdigest()] = message
+            stored_bets[author] = self.initiate_object_author(ctx.author)
+        if self.hash_function.hexdigest() not in stored_bets[author]['bets']:
+            self.hash_function.update(message.encode('utf-8'))
+            stored_bets[author]['bets'][self.hash_function.hexdigest()] = message
 
             create_json_file(self.storage_path, stored_bets)
             
@@ -37,9 +37,36 @@ class Bet(commands.Cog):
             await ctx.send(f'prédiction déjà mémorisée')
 
 
+    @commands.command()
+    async def show(self, ctx):
+        stored_bets = read_json_file(self.storage_path)
+        for user, data in stored_bets.items():
+            title = 'Prédiction'
+            if len(data['bets'].keys()) > 1:
+                title += 's'
+            embed = Embed(title=title)
+            embed.set_author(name=data['infos']['name'], icon_url=data['infos']['avatar'])
+
+            for id_bet, bet in data['bets'].items():
+                embed.add_field(name=f"prédiction ID {id_bet}", value=bet + '\n', inline=False)
+
+        await ctx.send(embed=embed)
+
+
+
     def check_storage(self):
         if not os.path.exists(self.storage_path):
             create_json_file(self.storage_path, {})
+
+    def initiate_object_author(self, member):
+        return {
+            'bets': {},
+            'infos': {
+                'avatar': member.avatar.url,
+                'name': member.name,
+            },
+        }
+
 
 async def setup(bot):
     await bot.add_cog(Bet(bot))

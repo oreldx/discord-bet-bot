a43f04f84992dcc73e27c42bcc9bd0b4aa4e4a4a
oreldx <orel.dev@proton.me>
2023-07-03 09:01:19 +0200

    ♻️ UPDATE format outputs

--- cogs/bet.py
+++ cogs/bet.py
@@ -4,8 +4,7 @@ from dotenv import dotenv_values
 
 import os
 import asyncio
-
-from hashlib import blake2s
+from hashlib import md5
 from datetime import datetime
 import locale
 
@@ -20,7 +19,6 @@ class Bet(commands.Cog):
         self.storage_path = env.get("STORAGE_PATH")
         self.check_storage()
 
-        self.hash_function = blake2s(digest_size=10)
         self.choices = {
             'binary': { 
                 'negative': '🔴',
@@ -36,95 +34,93 @@ class Bet(commands.Cog):
         author = str(ctx.author.id)
 
         if author not in stored_bets:
-            stored_bets[author] = self.init_better(ctx.author)
+            stored_bets[author] = self.init_author(ctx.author)
 
-        if self.hash_function.hexdigest() not in stored_bets[author]['bets']:
+        content = content.strip()
+        bet_type = content.split(' ')[0]
+        if bet_type in self.choices:
+            content = content[len(bet_type)-1:]
+        else: 
+            bet_type = self.default_bet_type
+        
+        bet_hash = md5(content.encode('utf-8')).hexdigest()
+        existing_bet = bet_hash in stored_bets[author]['bets']
 
-            content = content.strip()
-            bet_type = content.split(' ')[0]
-            if bet_type in self.choices:
-                content = content[len(bet_type)-1:]
-            else: 
-                bet_type = self.default_bet_type
-
-            self.hash_function.update(content.encode('utf-8'))
-            bet_hash = self.hash_function.hexdigest()
+        if existing_bet:
+            await ctx.send(self.format_output_create(existing_bet))
+        else:
             current_timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
             stored_bets[author]['bets'][bet_hash] = self.init_bet(content, current_timestamp, bet_type)
             create_json_file(self.storage_path, stored_bets)
 
-            await ctx.send(f'prédiction mémorisée, ID: {self.hash_function.hexdigest()}')
-
-            embed = Embed(title=f':game_die: NOUVELLE PRÉDICTION :sparkles:', description= f':crystal_ball: -{content}')
-            embed.set_footer(text=f"{bet_hash} créée par {author}")
-
-            message = await ctx.send(embed=embed)
+            message = await ctx.send(embed=self.format_output_create(existing_bet, bet_hash, author, content))
 
             for choice_emoji in self.choices[bet_type].values():
                 await message.add_reaction(choice_emoji)
-
-        else:
-            await ctx.send(f'prédiction déjà mémorisée')
 
     @commands.command()
     async def delete(self, ctx, bet_hash):
         stored_bets = read_json_file(self.storage_path)
         author = str(ctx.author.id)
-        if author in stored_bets and bet_hash in stored_bets[author]['bets']:
+
+        possible_deletion = author in stored_bets and bet_hash in stored_bets[author]['bets']
+
+        if possible_deletion:
             stored_bets[author]['bets'].pop(bet_hash)
             create_json_file(self.storage_path, stored_bets)
 
-            await ctx.send(f'prédiction {bet_hash} supprimée')
-        else:
-            await ctx.send(f"aucune prédiction correspondate à l'utilisateur prédiction ou ID de la prédiction incorrect")
-
+        await ctx.send(self.format_output_delete(possible_deletion, bet_hash))
+        
     @commands.command()
     async def show(self, ctx):
         stored_bets = read_json_file(self.storage_path)
 
-        for data in stored_bets.values():
-            title = 'Prédiction'
-            if len(data['bets'].keys()) > 1:
-                title += 's'
-            embed = Embed(title=title)
-            embed.set_author(name=data['infos']['name'], icon_url=data['infos']['avatar'])
+        data = []
+        for author in stored_bets.values():
+            author_data = {}
+            
+            author_data['name'] = author['infos']['name']
+            author_data['avatar'] = author['infos']['avatar']
+            author_data['bets'] = []
+            for bet in author['bets'].values():
+                bet_data = {}
 
-            for bet in data['bets'].values():
                 timestamp = datetime.strptime(bet['timestamp'], '%Y-%m-%d %H:%M:%S')
-                timestamp = timestamp.strftime("%Y-%m-%d")
-                prediction = bet['content']
-                embed.add_field(name=f'[{timestamp}] - {prediction}', value='', inline=False)
+                bet_data['timestamp'] = timestamp.strftime("%Y-%m-%d")
+                bet_data['content'] = bet['content']
+                bet_data['choices'] = []
 
                 emojis = self.choices[bet['bet_type']]
-
                 for emoji in emojis:
-                    if len(bet[emoji]) > 0:
-                        users = [await self.bot.fetch_user(user) for user in bet[emoji]]
-                        users = [user.name for user in users]
-                        users = (', ').join(users)
-                        name = f'|       {emojis[emoji]} {emoji}'
-                        embed.add_field(name=name, value=users, inline=False)
+                    if len(bet['choices'][emoji]) > 0:
+                        choice_data = {}
+                        users = [await self.bot.fetch_user(user) for user in bet['choices'][emoji]]
+                        choice_data['users'] = [user.name for user in users]
+                        choice_data['choice_emoji'] = emojis[emoji]
+                        choice_data['choice_value'] = emoji
 
-        await ctx.send(embed=embed)
+                        bet_data['choices'].append(choice_data)
+
+                author_data['bets'].append(bet_data)
+
+            data.append(author_data)
+
+        for embed in self.format_output_show(data):
+            await ctx.send(embed=embed) 
 
     @commands.command()
     async def list(self, ctx):
         stored_bets = read_json_file(self.storage_path)
         author = str(ctx.author.id)
-        
-        embed = Embed()
-        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
+        author_name = ctx.author.name
+        author_picture = ctx.author.avatar.url
+
+        bets = {}
         if author in stored_bets and len(stored_bets[author]['bets']) > 0:
+            bets = {bet_hash: bet['content'] for bet_hash, bet in stored_bets[author]['bets'].items()}
 
-            for bet_hash, bet in stored_bets[author]['bets'].items():
-                embed.add_field(name=bet_hash, value=bet['content'], inline=False)
-
-            await ctx.send(embed=embed)
-        else:
-            embed.add_field(name="Pas de prédictions trouvées", value="", inline=False)
-            await ctx.send(embed=embed)
-
+        await ctx.send(embed=self.format_output_list(author_name, author_picture, bets))
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -153,14 +149,14 @@ class Bet(commands.Cog):
 
                 if emoji in emojis:
                     # ADD new choice
-                    if reacting_user not in bet[emojis[emoji]]:
-                        bet[emojis[emoji]].append(reacting_user)
+                    if reacting_user not in bet['choices'][emojis[emoji]]:
+                        bet['choices'][emojis[emoji]].append(reacting_user)
                     emojis.pop(emoji)
 
                     # DEL old possible choice
                     for other_emoji in emojis.values():
-                        if reacting_user in bet[other_emoji]:
-                            bet[other_emoji].remove(reacting_user)
+                        if reacting_user in bet['choices'][other_emoji]:
+                            bet['choices'][other_emoji].remove(reacting_user)
                             break
 
                     create_json_file(self.storage_path, stored_bets)
@@ -170,7 +166,7 @@ class Bet(commands.Cog):
         if not os.path.exists(self.storage_path):
             create_json_file(self.storage_path, {})
 
-    def init_better(self, member):
+    def init_author(self, member):
         return {
             'bets': {},
             'infos': {
@@ -187,6 +183,58 @@ class Bet(commands.Cog):
                 'bet_type': bet_type,
                 'choices': {choice: [] for choice in self.choices[bet_type].keys()}
             }
+    
+    def format_output_create(self, exisiting_bet: bool, bet_hash: str = '', author: str = '', content: str = ''):
+        if exisiting_bet:
+            return 'prédiction déjà mémorisée'
+        else:
+            embed = Embed(title=f':game_die: NOUVELLE PRÉDICTION :sparkles:', description= f':crystal_ball: -{content}')
+            embed.set_footer(text=f"{bet_hash} créée par {author}")
+            return embed
+
+    def format_output_show(self, data: list):
+        embeds = []
+        
+        for author in data:
+            title = 'Prédiction'
+            if len(author['bets']) > 1:
+                title += 's'
+            embed = Embed(title=title)
+
+            embed.set_author(name=author['name'], icon_url=author['avatar'])
+
+            for bet in author['bets']:
+                timestamp = bet['timestamp']
+                prediction = bet['content']
+                embed.add_field(name=f'[{timestamp}] - {prediction}', value='', inline=False)
+
+                for choice in bet['choices']:
+                    users = (', ').join(choice['users'])
+                    name = f'|       {choice["choice_emoji"]} {choice["choice_value"]}'
+                    embed.add_field(name=name, value=users, inline=False)
+
+            embeds.append(embed)
+
+        return embeds
+
+    def format_output_list(self, author_name: str, author_picture: str, bets: dict,):
+        embed = Embed()
+        embed.set_author(name=author_name, icon_url=author_picture)
+
+        for bet_hash, bet in bets.items():
+            embed.add_field(name=bet_hash, value=bet, inline=False)
+        
+        if len(bets.keys()) == 0:
+            embed.add_field(name="Pas de prédictions trouvées", value="", inline=False)
+
+        return embed
+
+    def format_output_delete(self, deletion_return: bool, bet_hash: str):
+        if deletion_return:
+            return f'prédiction {bet_hash} supprimée'
+        else:
+            return f"aucune prédiction correspondate à l'utilisateur prédiction ou ID de la prédiction incorrect"
+
 
 async def setup(bot):
     await bot.add_cog(Bet(bot))
